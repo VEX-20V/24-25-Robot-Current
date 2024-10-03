05c46b50b3950835886391b46c13a96900e93d9e
LuketheName56 <LuketheName56@gmail.com>
2024-10-02 22:31:35 -0700

    Delete setUp.cpp

--- src/setUp.cpp
+++
@@ -1,8 +0,0 @@
-#include "lemlib/api.hpp" // IWYU pragma: keep
-#include "main.h"
-#include "lemlib/chassis/trackingWheel.hpp"
-
-
-// motor groups
-pros::MotorGroup leftMotors({-11, -12, -13}, pros::MotorGearset::blue); // left motor group
-pros::MotorGroup rightMotors({1, 2, 3}, pros::MotorGearset::blue); // right motor group - all reversed. 
