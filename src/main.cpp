e733ce27b7002ae60b5ca8a2de25e3d99e8307b0
LuketheName56 <LuketheName56@gmail.com>
2024-10-03 14:36:36 -0700

    Starting Linear PID Tuning

--- src/main.cpp
+++ src/main.cpp
@@ -155,8 +155,8 @@ void autonomous() {
 
     // set position to x:0, y:0, heading:0
     chassis.setPose(0, 0, 0);
-    // turn to face heading 90 with a very long timeout
-    chassis.turnToHeading(90, 100000);
+
+    chassis.moveToPose(0, 24, 0, 10000);
 
     /*
     // set chassis pose
