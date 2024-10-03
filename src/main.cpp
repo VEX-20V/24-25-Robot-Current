87771e6f0aeec0d83e87458dca75f973185ad60b
LuketheName56 <LuketheName56@gmail.com>
2024-10-03 14:20:51 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -52,17 +52,17 @@ lemlib::ControllerSettings linearController(10, // proportional gain (kP)
                                             20 // maximum acceleration (slew)
 );
 
-// angular motion controller
 lemlib::ControllerSettings angularController(2, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in inches
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in inches
-                                              0, // large error range timeout, in milliseconds
+                                              12, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
+
 // sensors for odometry
 lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel
                             nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
@@ -153,9 +153,13 @@ ASSET(BasicPathPt2_txt);
 
 
 void autonomous() {
+
+void autonomous() {
+    // set position to x:0, y:0, heading:0
     chassis.setPose(0, 0, 0);
-
-
+    // turn to face heading 90 with a very long timeout
+    chassis.turnToHeading(90, 100000);
+}
     /*
     // set chassis pose
     autonIntake(1);
